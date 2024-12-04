de76f3b54e9d2cbf2503fc93d654418a13258ea7
xuhuit <2963094337@qq.com>
2024-12-04 20:44:06 +0800

    部署Vue项目到GitHub Pages: 添加项目文件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <el-button>引入Element-plus的Vue项目</el-button>
+    <!-- 添加 ButtonExamples 组件 -->
+    <ButtonExamples />
+    <!-- 使用 InputExample 组件 -->
+    <InputExample />
+
+    <!-- 使用 SelectExample 组件 -->
+    <SelectExample />
+
+    <!-- 使用 TableExample 组件 -->
+    <TableExample />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 正确引入 Element Plus 的 ElButton 组件
+import {ElButton} from 'element-plus'
+// 引入 ButtonExamples 组件
+import ButtonExamples from './components/ButtonExamples.vue'
+// 引入自定义组件
+import InputExample from './components/InputExample.vue';
+import SelectExample from './components/SelectExample.vue';
+import TableExample from './components/TableExample.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ElButton, // 使用 Element Plus 的 ElButton 组件
+    ButtonExamples, // 注册 ButtonExamples 组件
+    InputExample, // 注册 InputExample 组件
+    SelectExample, // 注册 SelectExample 组件
+    TableExample // 注册 TableExample 组件
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css' // 确保在插件之前引入样式
 
-createApp(App).mount('#app')
+// 创建 Vue 应用实例
+const app = createApp(App)
+
+// 使用 Element Plus 插件
+app.use(ElementPlus)
+
+// 挂载 Vue 应用到 DOM 元素 '#app'
+app.mount('#app')
